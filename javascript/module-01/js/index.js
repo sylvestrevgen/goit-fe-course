8ffe7e3ecd235cc18af0b8cc4a3b740be1b7a7ff
Yevhenii Sylvestr <sylvestrevgen@gmail.com>
2019-04-15 11:29:59 +0300

    Fixed module-01 homework

--- javascript/module-01/js/index.js
+++ javascript/module-01/js/index.js
@@ -3,80 +3,74 @@ TASK #1
 */
 
 const ADMIN_PASSWORD = 'm4ng0h4ckz';
+let userPassword;
 let message;
 
-message = prompt('Введите пароль: ');
-if (message === null) {
-  alert('Отменено пользователем!');
-} else if (message === ADMIN_PASSWORD) {
-  alert('Добро пожаловать!');
+userPassword = prompt('Введите пароль: ');
+if (userPassword === null) {
+  message = 'Отменено пользователем!';
+} else if (userPassword === ADMIN_PASSWORD) {
+  message = 'Добро пожаловать!';
 } else {
-  alert('Доступ запрещен, неверный пароль!');
+  message = 'Доступ запрещен, неверный пароль!';
 }
+alert(message);
 
 /*
 TASK #2
 */
 
-// let credits = 23580;
-// const pricePerDroid = 3000;
-// let totalPrice;
+let credits = 23580;
+const pricePerDroid = 3000;
+let totalPrice;
 
-// let userBuy = prompt('Какое количество дроидов вы бы хотели приобрести?');
+let userBuy = prompt('Какое количество дроидов вы бы хотели приобрести?');
 
-// if (userBuy === null) {
-//   console.log('Отменено пользователем!');
-// } else {
-//   totalPrice = userBuy * pricePerDroid;
-//   if (totalPrice > credits) {
-//     console.log('Недостаточно средств на счету!');
-//   } else {
-//     credits = credits - totalPrice;
-//     console.log(`Вы купили ${userBuy} дроидов, на счету осталось ${credits} кредитов.`);
-//   }
-// }
+if (userBuy === null) {
+  console.log('Отменено пользователем!');
+} else {
+  totalPrice = userBuy * pricePerDroid;
+  if (totalPrice > credits) {
+    console.log('Недостаточно средств на счету!');
+  } else {
+    console.log(`Вы купили ${userBuy} дроидов, на счету осталось ${credits - totalPrice} кредитов.`);
+  }
+}
 
 /*
 TASK #3
 */
 
-// let userDelivery = prompt("Укажите страну в которую нужна доставка!");
-// userDelivery = userDelivery.toLowerCase();
-// let deliveryMessage;
-// let price;
+let userDelivery = prompt("Укажите страну в которую нужна доставка!");
+userDelivery = userDelivery.toLowerCase();
+const priceChina = 100;
+const priceSouthAmerica = 250;
+const priceAustralia = 170;
+const priceIndia = 80;
+const priceJamaica = 120;
 
-// switch (userDelivery) {
-//   case "китай":
-//     price = 100;
-//     deliveryMessage = `Доставка в ${userDelivery} будет стоить ${price} кредитов`;
-//     console.log(deliveryMessage);
-//     break;
+switch (userDelivery) {
+  case "китай":
+    console.log(`Доставка в ${userDelivery} будет стоить ${priceChina} кредитов`);
+    break;
 
-//   case "южная америка":
-//     price = 250;
-//     deliveryMessage = `Доставка в ${userDelivery} будет стоить ${price} кредитов`;
-//     console.log(deliveryMessage);
-//     break;
+  case "южная америка":
+    console.log(`Доставка в ${userDelivery} будет стоить ${priceSouthAmerica} кредитов`);
+    break;
 
-//   case "австралия":
-//     price = 170;
-//     deliveryMessage = `Доставка в ${userDelivery} будет стоить ${price} кредитов`;
-//     console.log(deliveryMessage);
-//     break;
+  case "австралия":
+    console.log(`Доставка в ${userDelivery} будет стоить ${priceAustralia} кредитов`);
+    break;
 
-//   case "индия":
-//     price = 80;
-//     deliveryMessage = `Доставка в ${userDelivery} будет стоить ${price} кредитов`;
-//     console.log(deliveryMessage);
-//     break;
+  case "индия":
+    console.log(`Доставка в ${userDelivery} будет стоить ${priceIndia} кредитов`);
+    break;
 
-//   case "ямайка":
-//     price = 120;
-//     deliveryMessage = `Доставка в ${userDelivery} будет стоить ${price} кредитов`;
-//     console.log(deliveryMessage);
-//     break;
+  case "ямайка":
+    console.log(`Доставка в ${userDelivery} будет стоить ${priceJamaica} кредитов`);
+    break;
 
-//   default:
-//     console.log("В вашей стране доставка недоступна!");
-//     break;
-// }
+  default:
+    console.log("В вашей стране доставка недоступна!");
+    break;
+}
